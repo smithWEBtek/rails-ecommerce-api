ad401b140db6c9bc1187c4311c0a20f3e86f8c55
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 11:07:48 -0400

    finish LineItem validation and relationship specs

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -4,13 +4,29 @@ RSpec.describe LineItem, type: :model do
   
   describe 'validations' do 
 
-    it 'requires a cart, item and quantity'
+    it 'requires a cart, item' do 
+      line_item = LineItem.new
+
+      expect(line_item.valid?).to eq(false) 
+      expect(line_item.errors.full_messages).to eq([
+        "Cart must exist",
+        "Item must exist"
+      ]) 
+    end
   end
 
   describe 'relationships' do 
 
-    it 'belongs to an item' 
+    it 'belongs to an item' do 
+      line_item = create(:line_item)
+      
+      expect(line_item.item.title).to eq("Legendary Link Hoodie")
+    end
 
-    it 'belongs to a cart'
+    it 'belongs to a cart' do 
+      line_item = create(:line_item)
+      
+      expect(line_item.cart.id).not_to eq(nil)
+    end
   end
 end
