fac7e690ea51e060d6051979616bcaf02c4882af
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 09:50:11 -0400

    finish and passing hash password upon User creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,8 +42,12 @@ RSpec.describe User, type: :model do
 
   describe 'on save' do 
 
-    it 'hashes a password' 
+    it 'hashes a password' do 
+      user = build(:user)
+      user.save
 
+      expect(user.password_digest).not_to equal(user.password)
+    end
   end 
 
   describe 'relationships' do 
