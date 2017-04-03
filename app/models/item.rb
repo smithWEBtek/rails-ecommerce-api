1d6a08d521cdbdbbb99906bffd7816149361d84d
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 11:07:27 -0400

    update Item relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
+  has_many :line_items, dependent: :destroy
 
   validates :title, :inventory, :price, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Item, type: :model do
   describe 'validations' do 
 
     it 'requires a title, inventory count, price and a category' do 
-      item = Item.new 
+      item = Item.new
 
       expect(item.valid?).to eq(false) 
       expect(item.errors.full_messages).to eq([
@@ -30,7 +30,7 @@ RSpec.describe Item, type: :model do
       item = create(:item)
       line_item = cart.line_items.create(quantity: 1, item: item)
 
-      expect(item.line_item.id).not_to eq(nil)
+      expect(item.line_items.first.id).not_to eq(nil)
 
       item.destroy
       line_item = LineItem.find_by(id: line_item.id)
