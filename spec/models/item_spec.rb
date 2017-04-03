ac041e655c139a1c3f0da64754410e6da37c0544
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 10:54:41 -0400

    add final relationship tests to Item spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,15 +15,29 @@ RSpec.describe Item, type: :model do
         "Price can't be blank"
       ]) 
     end
-    
   end
 
   describe 'relationships' do 
 
-    it 'belongs to a category' 
+    it 'belongs to a category' do 
+      item = create(:item)
+      
+      expect(item.category.title).to eq("clothing")
+    end
 
-    it 'has many line items' 
+    it 'has many line items that are destroyed upon deletion of item' do 
+      cart = create(:cart)
+      item = create(:item)
+      line_item = cart.line_items.create(quantity: 1, item: item)
 
-    it 'has many order items'
+      expect(item.line_item.id).not_to eq(nil)
+
+      item.destroy
+      line_item = LineItem.find_by(id: line_item.id)
+      
+      expect(line_item).to eq(nil)
+    end
+
+    it 'has many order items' 
   end
 end
