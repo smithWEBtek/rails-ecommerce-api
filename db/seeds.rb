b771a1ec563cd4e706565f28f72ac8d9e325d69e
smithwebtek <brad@smithwebtek.com>
2017-04-03 12:30:41 -0400

    start seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
- test change
+DATA_items = {
+  :item_keys =>
+    ["title", "inventory", "price", "category_id"],
+  :items ="> [
+    ["Zelda 7 Rupee Set", 3, "$19.95", 1],
+    ["The Zelda inspired wall clock", 3, "$38.88", 1],
+    ["Map of Hyrule from Zelda", 3, "$20.00", 1],
+    ["Nintendo Switch Padded Dock Cover", 3, "$13.00", 1],
+    ["Zelda Prop Kit", 3, "$30.00", 1],
+    ["Ready to Ship! Zelda Key Holder", 3, "$15.00", 1],
+    ["Zelda Inspired Majora Mask", 3, "$129.6", 1],
+    ["The Zelda travel sized amiibo", 3, "$8.50 ", 1], 
+    ["Rusted Master Sword Miniature Prop", 3, "$19.40", 1] 
+  ]
+}
+
+def make_items
+  DATA_items[:items].each do |item|
+    new_item = Item.new
+    item.each_with_index do |attribute, i|
+      new_item.send(DATA_items[:item_keys][i]+"=", attribute)
+    end
+    new_item.save
+  end
+end
+ 
+
+def main
+    make_items
+end
