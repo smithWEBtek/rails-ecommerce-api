128271708d3a083107a83190ea179c5b6f8d90f0
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 10:04:12 -0400

    add Cart modle migrations

---
+++ db/migrate/20170403135727_create_carts.rb
@@ -0,0 +1,10 @@
+class CreateCarts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :carts do |t|
+      t.references :user, foreign_key: true
+      t.string :status, default: "Active"
+
+      t.timestamps
+    end
+  end
+end
