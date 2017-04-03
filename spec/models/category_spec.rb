ff1b9596d6d30e5de9bdfce348d2f1682e578d27
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 10:38:03 -0400

    add validations to Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe Category, type: :model do
   
   describe 'validations' do
 
-    it 'requires a title'
+    it 'requires a title' do 
+      category = build(:category, title: nil)
+
+      expect(category.valid?).to eq(false)
+      expect(category.errors.full_messages).to eq([
+        "Title can't be blank"
+      ])
+    end
   end
 
   describe 'relationships' do 
