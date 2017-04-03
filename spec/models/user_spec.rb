93608126dd6541163a1268b174cd865220a9a6cf
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 09:32:54 -0400

    add initial User model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  describe 'validations' do
+
+    it 'requires a email and password upon creation' 
+
+    it 'requires that an email is unique'
+
+  end
+
+  describe 'relationships' do 
+
+    it 'has one cart'
+
+    it 'has many orders'
+
+  end
+end
