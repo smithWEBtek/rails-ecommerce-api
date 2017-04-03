2e49150a2bf20c7ee02c447d3f6b977dc4e5ebec
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 10:35:46 -0400

    add initial specs for Category

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  
+  describe 'validations' do
+
+    it 'requires a title'
+  end
+
+  describe 'relationships' do 
+
+    it 'has many items'
+  end
+end
