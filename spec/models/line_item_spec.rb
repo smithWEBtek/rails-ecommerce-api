2221af71fd3e0dfc7932bc29f4cef14b1e629a1d
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 10:58:05 -0400

    add initial LineItem specs

---
+++ spec/models/line_item_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe LineItem, type: :model do
+  
+  describe 'validations' do 
+
+    it 'requires a cart, item and quantity'
+  end
+
+  describe 'relationships' do 
+
+    it 'belongs to an item' 
+
+    it 'belongs to a cart'
+  end
+end
