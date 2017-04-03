83806e25b43bd7d1313c2ace94ac3b3452df5383
Luke Ghenco <lukeghenco@gmail.com>
2017-04-03 10:48:01 -0400

    add presence validators to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :category
+
+  validates :title, :inventory, :price, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,18 @@ RSpec.describe Item, type: :model do
   
   describe 'validations' do 
 
-    it 'requires a title, inventory count, price and a category' 
+    it 'requires a title, inventory count, price and a category' do 
+      item = Item.new 
+
+      expect(item.valid?).to eq(false) 
+      expect(item.errors.full_messages).to eq([
+        "Category must exist",
+        "Title can't be blank",
+        "Inventory can't be blank",
+        "Price can't be blank"
+      ]) 
+    end
+    
   end
 
   describe 'relationships' do 
